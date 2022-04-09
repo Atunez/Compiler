7b27ca7ed6873c13bae1e286909f2cf8991435f5
Abdel Issa <issaa@appstate.edu>
2022-04-09 19:06:09 -0400

    Fix buildAST and finish buildTree

--- RegexParser.py
+++ RegexParser.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 class Node:
     children = list()
     value = -1
@@ -13,7 +15,7 @@ class Node:
     def __str__(self):
         output = self.label
         if self.children is None:
-            return output
+            return output 
         for i in range(len(self.children)):
             output += "{" + str(self.children[i]) + "}"
         return output
@@ -49,35 +51,50 @@ def parseIntoTokens(regex):
     return tokens
 
 # Assume a "valid" regex
-def buildAST(regex, start):
+def buildAST(regex, start, counter):
     AST = Node(list(), -1, "Conc")
-    i, c = start, 1
+    i, c = start, counter
     while i < len(regex):
         match regex[i]:
             case "(":
-                rAST, i = buildAST(regex, i+1)
+                rAST, i, c = buildAST(regex, i+1, c)
                 AST.children.extend(rAST.children)
             case ")":
-                return AST, i+1
+                return AST, i+1, c
             case "*":
                 AST.children.append(Node([AST.children.pop(-1)], -1, "Star"))
                 i += 1
             case "|":
                 prevNode = AST.children.pop(-1)
-                nextNode, i = buildAST(regex, i+1)
+                nextNode, i, c = buildAST(regex, i+1, c)
                 toAdd = list()
                 toAdd.append(prevNode)
                 toAdd.extend(nextNode.children)
                 AST.children.append(Node(toAdd , -1, "Or"))
-                return AST, i
+                return AST, i, c
             case _:
                 unit = Node(list(), c, regex[i])
                 c += 1
                 i += 1
                 AST.children.append(unit)
-    return AST, -1
+    return AST, -1, c
 
-#testRegex = "(a|b)*abb#"
-testRegex = "ab*c(a|b*)(d)d|e"
-AST, _ = buildAST(parseIntoTokens(testRegex), 0)
+def buildTree(AST):
+    if len(AST) == 1:
+        return AST[0]
+    # Need some queue or stack so that
+    # Take off 2, make new node, put back in...
+    # we will reuse the Node class st: children[0] is left (or single...) 
+    # and children[1] is right
+    rTree = buildTree(AST[:-1])
+    return Node([rTree, AST[-1]], -1, "Conc")
+        
+    
+        
+        
+
+testRegex = "(a|b)*abb#"
+AST, _, _ = buildAST(parseIntoTokens(testRegex), 0, 1)
+binaryAST = buildTree(AST.children)
 print(AST)
+print(binaryAST)
